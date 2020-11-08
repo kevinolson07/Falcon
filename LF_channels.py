e14751e7d11767be2928f6eb2efee546f700008f
kevinolson07 <kevin.olson07@gmail.com>
2020-11-07 23:01:23 -0800

    added command byte

--- LF_channels.py
+++ LF_channels.py
@@ -10,20 +10,20 @@ two = 0xA0
 three = 0xFA
 
 def write_pot(input):
-    msb = input >> 8 
+    msb = 0x13
     lsb = input & 0xFF
-    spi.xfer([input])
+    spi.xfer([msb,lsb])
     
 
 while True:
-    # pot_value = int(input("choose 1, 2, or 3:") 
-    write_pot(0x11)
-    time.sleep(.005)
-    # if pot_value == 1:
-    #     write_pot(one)
-    # elif pot_value == 2:
-    #     write_pot(two)
-    # elif pot_value == 3:
-    #     write_pot(three)
-    # else:
-    #     write_pot(0x00)
+    pot_value = int(input("choose 1, 2, or 3:"))
+    # write_pot(0xFA)
+    
+    if pot_value == 1:
+        write_pot(one)
+    elif pot_value == 2:
+        write_pot(two)
+    elif pot_value == 3:
+        write_pot(three)
+    else:
+        write_pot(0x00)
