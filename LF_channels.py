4bdf6575c194e531d8e904fb1c61219b4ccd64ba
kevinolson07 <kevin.olson07@gmail.com>
2020-11-16 18:42:38 -0800

    added bank selection to manual mode and added all channels to Auto

--- LF_channels.py
+++ LF_channels.py
@@ -4,12 +4,16 @@ import RPi.GPIO as GPIO
 
 channel_1 = 16
 channel_2 = 18
+all_channels = (16,18)
 
 # Hex values for equivalant resistance
-one = 0x0D
-two = 0x16
-three = 0x1C
-four = 0x23
+one = 0x0D      #625 Ohms
+two = 0x16      #910 Ohms
+three = 0x1C        #1150 Ohms 
+four = 0x23     #1385 Ohms
+bank_1 = 0x11
+bank_2 = 0x12
+all_banks = 0x13
 
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -20,105 +24,124 @@ GPIO.setup(channel_2,GPIO.OUT)
 GPIO.output(channel_1, GPIO.HIGH)
 GPIO.output(channel_2, GPIO.HIGH)
                                                                                                                                                                         
-def write_pot(input):
-    msb = 0x13
+def write_pot(input,bank_select):
+    msb = bank_select
     lsb = input & 0xFF
     spi.xfer([msb,lsb])
 
-def selection(loop, channel_select):
+def settings(channel_select, mode, bank_select):
     x = 1
     while x <= 1:
-        if loop == 1:
+        if mode == 1:
+            dwell_time = int(input("Dwell time in seconds:"))
+            cycles = int(input("Enter number of cycles:"))
             try:
-                dwell_time = int(input("Dwell time in seconds:"))
-                GPIO.output(channel_select, GPIO.LOW)
-                write_pot(one)
-                GPIO.output(channel_select, GPIO.HIGH)
-                time.sleep(dwell_time)
-                GPIO.output(channel_select, GPIO.LOW)
-                write_pot(two)
-                GPIO.output(channel_select, GPIO.HIGH)
-                time.sleep(dwell_time)
-                GPIO.output(channel_select, GPIO.LOW)
-                write_pot(three)
-                GPIO.output(channel_select, GPIO.HIGH)
-                time.sleep(dwell_time)
-                GPIO.output(channel_select, GPIO.LOW)
-                write_pot(four)
-                GPIO.output(channel_select, GPIO.HIGH)
-                time.sleep(dwell_time)
-                print("completed auto")
-                x+=1
+                for c in range(cycles):
+                    bank_select = all_banks
+                    print(channel_select, mode)
+                    GPIO.output(all_channels, GPIO.LOW)
+                    write_pot(one,all_banks)
+                    GPIO.output(all_channels, GPIO.HIGH)
+                    time.sleep(dwell_time)
+                    GPIO.output(all_channels, GPIO.LOW)
+                    write_pot(two,all_banks)
+                    GPIO.output(all_channels, GPIO.HIGH)
+                    time.sleep(dwell_time)
+                    GPIO.output(all_channels, GPIO.LOW)
+                    write_pot(three,all_banks)
+                    GPIO.output(all_channels, GPIO.HIGH)
+                    time.sleep(dwell_time)
+                    GPIO.output(all_channels, GPIO.LOW)
+                    write_pot(four,all_banks)
+                    GPIO.output(all_channels, GPIO.HIGH)
+                    time.sleep(dwell_time)
+                    print("completed auto")
+                    x+=1
             except ValueError:
                 print("Invalid Value")
-                question1()
-        if loop == 2:
+                mode_select()
+        if mode == 2:
             pot_value = int(input("choose 1 (4mA), 2 (10mA), 3 (14mA), or 4 (20mA):"))
             if pot_value == 1:
                 GPIO.output(channel_select, GPIO.LOW)
-                write_pot(one)
+                write_pot(one, bank_select)
                 GPIO.output(channel_select, GPIO.HIGH)
                 x+=1
             elif pot_value == 2:
                 GPIO.output(channel_select, GPIO.LOW)
-                write_pot(two)
+                write_pot(two, bank_select)
                 GPIO.output(channel_select, GPIO.HIGH)
                 x+=1
             elif pot_value == 3:
                 GPIO.output(channel_select, GPIO.LOW)
-                write_pot(three)
+                write_pot(three, bank_select)
                 GPIO.output(channel_select, GPIO.HIGH)
                 x+=1
             elif pot_value == 4:
                 GPIO.output(channel_select, GPIO.LOW)
-                write_pot(four)
+                write_pot(four, bank_select)
                 GPIO.output(channel_select, GPIO.HIGH)
                 x+=1
             else:
                 print("Invalid Value")
-                question1()
-        else:
-            question1()
+        # else:
+        #     mode_select()
 
         
-def question(channel_select):
+def mode_select():
     x = 1
     while x <= 1:
         try:
-            loop = int(input("Type \'1\' for auto: \nType \'2\' for manual: "))
-            if loop == 1 and channel_select == 1:
+            mode = int(input("Type \'1\' for auto: \nType \'2\' for manual: "))
+            if mode == 1:
                 x+=1
-                selection(loop, channel_1)
-            if loop == 2 and channel_select == 1:
+                settings(None, mode, None)
+            if mode == 2:
                 x+=1
-                selection(loop, channel_1)
-            if loop == 1 and channel_select == 2:
-                x+=1
-                selection(loop, channel_2)
-            if loop == 2 and channel_select == 2:
-                x+=1
-                selection(loop, channel_2)
+                channel_select(mode)
             else:
                 print("Try again")
         except ValueError:
             print("Invalid Value")
 
-def question1():
+def channel_select(mode):
     i=1
     while i <= 1:
         try:
             channel_select = int(input("Channel 1 or 2?"))
-            if channel_select == 1 or channel_select == 2:
+            bank_select = int(input("Bank 1 or 2?"))
+            if channel_select == 1 and bank_select ==1:
+                channel_select = channel_1
+                bank_select = bank_1
                 i+=1
-                question(channel_select)
+                settings(channel_select,mode, bank_select)
+
+            if channel_select == 1 and bank_select == 2:
+                channel_select = channel_1
+                bank_select = bank_2
+                i+=1
+                settings(channel_select,mode,bank_select)
+
+            if channel_select == 2 and bank_select == 1:
+                channel_select = channel_2
+                bank_select = bank_1
+                i+=1
+                settings(channel_select,mode,bank_select)
+
+            if channel_select == 2 and bank_select == 2:
+                channel_select = channel_2
+                bank_select = bank_2
+                i+=1
+                settings(channel_select,mode,bank_select)
+
             else:
                 print("sorry, try again ")
         except ValueError:
-            print("Invalid Value")
+            print("Invalid Value1")
     
 
 while True:
-    question1()
+    mode_select()
    
     
     # write_pot(0xFA)
